3c884c83bbba5345207577ea4571b0f72048bb98
Hassan M. Abudu <meyian@gmail.com>
2018-02-23 18:15:44 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/*
+
+Vanilla Bootstrap
+
+*/
+
+$(function(){
+
+  // variables
+
+
+  // classes
+
+
+
+
+
+
+
+  // functions
+
+  function catastrophicFailure(strErrMsg){
+
+  }
+
+  function initHandlers(){
+    
+  }
+
+  function init(){
+    console.log('line 32. init');
+    initHandlers();
+  }
+
+
+
+  // script
+
+  init();
+
+})
